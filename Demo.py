fa67450ad600cc0d2c3bcf1f63c40396beee4ca5
akshitha123hub <121024936+akshitha123hub@users.noreply.github.com>
2022-12-29 00:25:10 +0530

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,3 +1,13 @@
 print("hello world")
 # new changes 
 print("hello 123")
+
+#haii i'm akshitha
+#let me add a program for checking odd or even?
+
+a=int(input("enter a number: ")
+if(a%2==0):
+    print("the number is even")
+else:
+    print("the number is odd")
+      
